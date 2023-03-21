f33e7e981a006748fde7b7398d4a8390bb8a3aaf
fathimasilviyak <silviyakvm@gmail.com>
2023-03-21 20:52:59 +0530

    Add data to table row by row

--- main.py
+++ main.py
@@ -5,12 +5,21 @@ from prettytable import PrettyTable
 table = PrettyTable()
 # print(table)
 
-# Add column to the table created
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
+# Add data column by column to the table created
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
 # print(table)
 
+# Add data row by row to the table created
+table.add_rows([
+    ["Pokemon Name", "Type"],
+    ["Pikachu", "Electric"],
+    ["Squirtle", "Water"],
+    ["Charmander", "Fire"],
+])
+print(table)
+
 # Change the alignment of the data in table to left aligned
-print(table.align)
+# print(table.align)
 table.align = "l"
 print(table)
